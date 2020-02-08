d2b55efa91701e17f528b70c85efddf36c45242e
jpegzilla <eris@jpegzilla.com>
2020-02-07 19:12:33 -0500

    added nomobile component

---
+++ src/components/NoMobile.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const NoMobile = () => {
+  return (
+    <section>
+      minerva's akasha does not yet exist for your device. if you want to help
+      make it a reality, get in touch with{" "}
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://twitter.com/jpegzilla"
+      >
+        jpegzilla.
+      </a>
+    </section>
+  );
+};
