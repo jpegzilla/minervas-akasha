4d532ccf82e3e820453489f4fa473dff7645df29
jpegzilla <eris@jpegzilla.com>
2020-02-07 19:11:26 -0500

    added color code util

---
+++ src/utils/structures/utils/colorcodes.js
@@ -0,0 +1,7 @@
+// these color codes are applied to data structures' html representations as css
+// classes, which are used to apply colors to the elements.
+// each value in this structure corresponds to a certain css class defined in
+// _colorcodes.sass.
+export const colorcodes = {
+  pink: "colorcode-pink"
+};
