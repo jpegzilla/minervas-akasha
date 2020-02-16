b06f362c5cbcbe5d6d2465d9cab644277c881b9f
jpegzilla <eris@jpegzilla.com>
2020-02-16 15:59:22 -0500

    added context menu

---
+++ src/components/ContextMenu.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { uuidv4 } from "./../utils/misc";
+
+export const ContextMenu = props => {
+  const { contextMenu, contextMenuElem } = props;
+  const { x, y } = contextMenu.position;
+  const [subItems, setSubItems] = useState({ display: false, id: null });
+
+  const [ctxMenuItems, setCtxMenuItems] = useState([
+    {
+      title: "example",
+      onClick: () => console.log("clicked item 1"),
+      id: 1
+    },
+    {
+      title: "example",
+      onClick: () => console.log("clicked item 2"),
+      id: 2,
+      showSubItems: false,
+      subItems: [
+        {
+          title: "example",
+          onClick: () => console.log("clicked subitem 1"),
+          id: "someid"
+        }
+      ]
+    },
+    {
+      title: "example",
+      onClick: () => console.log("clicked item 3"),
+      id: 3
+    }
+  ]);
+
+  const left = x + "px";
+  const top = y + "px";
+
+  // maybe use index for something?
+  const handleItemClicked = (e, item, idx) => {
+    e.stopPropagation();
+
+    item.onClick();
+  };
+
+  return (
+    <div
+      ref={contextMenuElem}
+      style={{
+        left,
+        top
+      }}
+      id="context-menu-container"
+    >
+      <header>context menu</header>
+      {ctxMenuItems.map((item, i) => (
+        <div
+          key={uuidv4()}
+          onClick={e => handleItemClicked(e, item, i)}
+          onMouseEnter={() => setSubItems({ display: true, id: i })}
+          onMouseLeave={() => setSubItems({ display: false, id: i })}
+          className="context-menu-item"
+        >
+          {item.title}
+          {/* sub item */}
+          {subItems.id === i &&
+            subItems.display &&
+            item.subItems &&
+            item.subItems.map(item => {
+              return (
+                <div
+                  key={uuidv4()}
+                  onClick={e => {
+                    e.stopPropagation();
+                    item.onClick(e);
+                  }}
+                  className={`context-menu-subitem ${
+                    subItems.display ? "active" : ""
+                  }`}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+        </div>
+      ))}
+    </div>
+  );
+};
