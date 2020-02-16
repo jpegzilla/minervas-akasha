ad1f05e775d345ffbc27a6462aa68a351efd2165
jpegzilla <eris@jpegzilla.com>
2020-02-16 16:05:15 -0500

    temp taskbar

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect, useRef } from "react";
 import { uuidv4 } from "./../utils/misc";
 
 export const Taskbar = props => {
-  const { minerva } = props;
+  const {
+    minerva,
+    setSettingsMenuOpen,
+    settingsMenuOpen,
+    sidebarOpen,
+    setSidebarOpen
+  } = props;
 
   const [activeWindow, setActiveWindow] = useState(null);
+
   const [windows, setWindows] = useState([
     {
       title: "default",
@@ -15,5 +22,42 @@ export const Taskbar = props => {
     }
   ]);
 
-  return <div>taskbar</div>;
+  const openSettingsMenu = () => {
+    setSettingsMenuOpen(!settingsMenuOpen);
+  };
+
+  const openSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  return (
+    <section id="application-taskbar">
+      <div
+        onClick={openSidebar}
+        className="application-square-buttons"
+        id="application-menu-button"
+      >
+        ⌂
+      </div>
+      <div
+        onClick={openSettingsMenu}
+        onMouseEnter={console.log}
+        className="application-square-buttons"
+        id="application-settings-button"
+      >
+        ⚙
+      </div>
+      <div id="application-windows-list">
+        {windows.map((item, i) => {
+          return (
+            <div
+              title={item.title}
+              key={`${i}${uuidv4}`}
+              className={`${item.type} ${item.size}`}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 };
