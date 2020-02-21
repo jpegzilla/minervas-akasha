6880ed5b713ee780c1ba91268a693ab498bf490f
jpegzilla <eris@jpegzilla.com>
2020-02-21 16:38:42 -0500

    added topbar component

---
+++ src/components/Topbar.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, useContext } from "react";
+
+export const Topbar = () => {
+  const time = () => {
+    let hours = new Date().getHours();
+
+    let minutes = new Date()
+      .getMinutes()
+      .toString()
+      .padStart(2, "0");
+    const seconds = new Date()
+      .getSeconds()
+      .toString()
+      .padStart(2, "0");
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    return `${hours.toString().padStart(2, "0")}:${minutes}:${seconds}`;
+  };
+
+  const [currentTime, setCurrentTime] = useState(time);
+
+  setInterval(() => {
+    setCurrentTime(time);
+  }, 1000);
+
+  return (
+    <section id="top-bar">
+      <div className="taskbar-button" id="settings-button">
+        settings
+      </div>
+      <b />
+
+      <div className="taskbar-button" id="topbar-clock">
+        {currentTime}
+      </div>
+    </section>
+  );
+};
