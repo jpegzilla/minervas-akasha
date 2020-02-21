675d03cf3f7f6ef8f4312334b02a2816ed653cb7
jpegzilla <eris@jpegzilla.com>
2020-02-21 16:40:33 -0500

    added rudimentary error boundary

---
+++ src/components/subcomponents/ErrorBoundary.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: false, info: "", message: "" };
+  }
+
+  componentDidCatch(err, info) {
+    this.setState({ error: true, info, message: err });
+
+    console.log(err, info);
+  }
+
+  render() {
+    if (this.state.error) return <h1>there's an error here! => {this.state.error}</h1>;
+    return this.props.children;
+  }
+}
