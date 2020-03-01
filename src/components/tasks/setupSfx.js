238b7f35eb46e3317fba84a20d885136e1de1e45
jpegzilla <eris@jpegzilla.com>
2020-03-01 18:59:07 -0500

    removed double click sound. it's so loud

--- src/components/tasks/setupSfx.js
+++ src/components/tasks/setupSfx.js
@@ -1,10 +1,9 @@
 export const setup = ctx => {
-  document.addEventListener("dblclick", e => {
-    if (e.repeat) return;
-
-    ctx.play("noise");
-  });
-
+  // document.addEventListener("dblclick", e => {
+  //   if (e.repeat) return;
+  //
+  //   ctx.play("noise");
+  // });
   // document.addEventListener("keydown", e => {
   //   if (e.repeat) return;
   //
