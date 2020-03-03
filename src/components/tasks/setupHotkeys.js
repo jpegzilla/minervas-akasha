351d428dccdd7db36934c566b64a86441bf71ec1
jpegzilla <eris@jpegzilla.com>
2020-03-02 21:27:45 -0500

    added test sound hotkey

--- src/components/tasks/setupHotkeys.js
+++ src/components/tasks/setupHotkeys.js
@@ -1,9 +1,20 @@
 import cheatcode from "./../../utils/cheatcodes";
 
+import keySound from "./../../assets/audio/computer/click-short.wav";
+import audiomanager from "./../../utils/audiomanager";
+
+const m = new audiomanager();
+// load key sound into audio manager for typing sound
+m.load([{ file: keySound, name: "key" }]);
+
 export default () => {
   const testHotkey = new cheatcode("a, s, d", () => {
     console.log("hotkey working!");
   });
 
-  [testHotkey].forEach(c => c.start());
+  const sound = new cheatcode("a", () => {
+    m.play("key");
+  });
+
+  [testHotkey, sound].forEach(c => c.start());
 };
