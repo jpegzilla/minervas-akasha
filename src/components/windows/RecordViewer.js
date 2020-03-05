44b37aa23488b03c50a6e07d50f4b45dbca87aff
jpegzilla <eris@jpegzilla.com>
2020-03-05 02:27:22 -0500

    added initial markup to record viewer

--- src/components/windows/RecordViewer.js
+++ src/components/windows/RecordViewer.js
@@ -5,11 +5,28 @@ import { globalContext } from "./../App";
 export const RecordViewer = props => {
   const { minerva } = useContext(globalContext);
 
-  console.log(props);
+  // useEffect(
+  //   () => {
+  //     console.log(minerva.windows);
+  //   },
+  //   [minerva.windows]
+  // );
+
   return (
     <div>
-      this is a record viewer. it contains all the records belonging to a
-      certain user.
+      <header>
+        <div className="record-viewer-navigation">navigation</div>
+        <div className="record-viewer-tabs">tabs</div>
+      </header>
+      <section className="record-viewer-main">
+        <section className="record-viewer-sidebar">
+          <div>sidebar</div>
+        </section>
+        <section className="record-viewer-records">
+          <div>records</div>
+        </section>
+      </section>
+      <footer className="record-viewer-footer">footer</footer>
     </div>
   );
 };
