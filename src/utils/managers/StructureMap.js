070724bfbd1270b22a07c00f633ef4cdf60c1995
jpegzilla <eris@jpegzilla.com>
2020-03-05 02:25:54 -0500

    added structure map

---
+++ src/utils/managers/StructureMap.js
@@ -0,0 +1,13 @@
+import { Shard } from "./../structures/shard";
+import { Grimoire } from "./../structures/grimoire";
+import { Node } from "./../structures/node";
+import { Hypostasis } from "./../structures/hypostasis";
+import { Athenaeum } from "./../structures/athenaeum";
+
+export default {
+  Shard: Shard,
+  Grimoire: Grimoire,
+  Node: Node,
+  Hypostasis: Hypostasis,
+  Athenaeum: Athenaeum
+};
