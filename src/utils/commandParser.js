ef4260734a1596a25a61b4a471f5a7d0ad1c2053
jpegzilla <eris@jpegzilla.com>
2020-03-06 17:26:14 -0500

    add structure command

--- src/utils/commandParser.js
+++ src/utils/commandParser.js
@@ -1,11 +1,63 @@
+import { uuidv4 } from "./../utils/misc";
+
+const validTypes = ["shard", "node", "grimoire", "athenaeum", "hypostasis"];
+
 // methods that are only for use by the system and privileged users.
 const MinervaMethods = {};
 
 // methods that are for use by anyone, including regular users.
-const UserMethods = {};
+const UserMethods = {
+  addStructure(type, setWindows, minerva) {
+    if (type === type.toLowerCase())
+      throw new Error("invalid type provided to addStructure");
+    // add new window to list
+    const ath = {
+      title: "datastructure",
+      state: "restored",
+      stringType: "Window",
+      component: "DataStructure",
+      componentProps: {
+        type,
+        structId: uuidv4()
+      },
+      belongsTo: minerva.user.id,
+      id: uuidv4(),
+      position: {
+        x: 100,
+        y: 100
+      }
+    };
 
-export const parseCommand = (command, setLog, minerva) => {
+    minerva.setWindows([...minerva.windows, ath]);
+
+    setWindows([...minerva.windows]);
+
+    console.log(minerva, type);
+  }
+};
+
+export const parseCommand = (command, setWindows, minerva) => {
   const username = minerva.user.name;
+
+  if (command.startsWith("add")) {
+    const params = command.split(" ");
+    params.splice(0, 1);
+
+    if (!validTypes.includes(params[0]))
+      return {
+        state: "error",
+        message:
+          "invalid parameter passed to add. first parameter must be a structure type."
+      };
+
+    UserMethods.addStructure(
+      params[0][0].toUpperCase() + params[0].slice(1),
+      setWindows,
+      minerva
+    );
+
+    return `added new ${params[0]}.`;
+  }
 
   switch (command) {
     case "hello":
