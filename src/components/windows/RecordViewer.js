a049abd457356c2fe93c9b4a3346c7a2311c38ef
jpegzilla <eris@jpegzilla.com>
2020-03-10 22:32:27 -0400

    handle opening existing record

--- src/components/windows/RecordViewer.js
+++ src/components/windows/RecordViewer.js
@@ -5,6 +5,40 @@ import { globalContext } from "./../App";
 export const RecordViewer = props => {
   const { minerva } = useContext(globalContext);
 
+  const handleOpenRecord = (e, item) => {
+    // make sure the window isn't already open
+    const foundItem = minerva.windows.find(
+      i =>
+        i.component === "DataStructure" && i.componentProps.structId === item.id
+    );
+
+    if (foundItem) return;
+
+    const itemToOpen = minerva.record.records[item.type].find(
+      i => i.id === item.id
+    );
+
+    const { id, type } = itemToOpen;
+
+    const struct = {
+      title: "datastructure",
+      state: "restored",
+      stringType: "Window",
+      component: "DataStructure",
+      componentProps: {
+        type,
+        structId: id
+      },
+      belongsTo: minerva.user.id,
+      id: uuidv4(),
+      position: {
+        x: 100,
+        y: 100
+      }
+    };
+
+    minerva.setWindows([...minerva.windows, struct]);
+
   // useEffect(
   //   () => {
   //     console.log(minerva.windows);
