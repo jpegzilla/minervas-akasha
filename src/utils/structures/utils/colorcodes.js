b491e502f9172b0415d1516a1040d1282e90c9bc
jpegzilla <eris@jpegzilla.com>
2020-03-17 19:26:35 -0400

    update colorcodes

--- src/utils/structures/utils/colorcodes.js
+++ src/utils/structures/utils/colorcodes.js
@@ -2,11 +2,12 @@
 // classes, which are used to apply colors to the elements.
 // each value in this structure corresponds to a certain css class defined in
 // _colorcodes.sass.
-export const colorcodes = {
+export default {
   pink: "colorcode-pink",
   white: "colorcode-white",
-  black: "colorcode-black",
   blue: "colorcode-blue",
+  red: "colorcode-red",
+  green: "colorcode-green",
   purple: "colorcode-purple",
   yellow: "colorcode-yellow"
 };
