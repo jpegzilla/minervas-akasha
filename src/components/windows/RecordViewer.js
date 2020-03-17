53e55117c13c9a027b5e6e0f4aee6ed465d1e0d0
jpegzilla <eris@jpegzilla.com>
2020-03-17 19:34:35 -0400

    functionality improvements to recordviewer

--- src/components/windows/RecordViewer.js
+++ src/components/windows/RecordViewer.js
@@ -7,7 +7,9 @@ import { globalContext } from "./../App";
 export const RecordViewer = props => {
   const { setWindows } = props;
   const { minerva } = useContext(globalContext);
+
   const [records, setRecords] = useState(minerva.record.records);
+  const [statusBar, setStatusBar] = useState();
 
   const [recordData, setRecordData] = useState({
     records: [],
@@ -78,7 +80,10 @@ export const RecordViewer = props => {
   );
 
   return (
-    <div className="record-viewer-container">
+    <div
+      onClick={() => void setStatusBar(false)}
+      className="record-viewer-container"
+    >
       <header>
         <div className="record-viewer-navigation">navigation</div>
         <div className="record-viewer-tabs">tabs</div>
@@ -92,21 +97,57 @@ export const RecordViewer = props => {
             // looks sort of like this: {id: uuidv4(), name: "node", data: {...}}
             const { type, tags, name } = item;
 
+            const nameToShow =
+              name === type ? type : name.substring(0, 7).padEnd(10, ".");
+
             // if a type starts with a vowel, use an instead of a when referring to it
             const aOrAn = ["a", "e", "i", "o", "u", "y"].some(item =>
               type.startsWith(item)
             )
               ? "an"
               : "a";
+            let tagsToShow;
 
-            const tagsToShow = tags.length
-              ? tags.map(tag => `"${tag.name}"`).join(", ")
-              : "none";
+            if (tags.length === 0) {
+              tagsToShow = "none";
+            } else if (tags.length > 3) {
+              tagsToShow =
+                tags
+                  .map(tag => `${tag.name}`)
+                  .slice(0, 3)
+                  .join(", ") + ` + ${tags.length - 3} more`;
+            } else if (tags.length > 0) {
+              tagsToShow = tags.map(tag => `${tag.name}`).join(", ");
+            }
+
+            let message = { title: "no file information" };
+
+            if (item.data) {
+              if (item.data.file) {
+                const { title, humanSize, mime, ext } = item.data.file;
+
+                message = {
+                  title,
+                  humanSize,
+                  mime,
+                  ext
+                };
+              }
+            }
 
             return (
               <div
                 key={`type-${i}-record`}
-                onDoubleClick={e => handleOpenRecord(e, item)}
+                onClick={e => {
+                  e.stopPropagation();
+
+                  const m = Object.values(message).join(" ⦚ ");
+
+                  return void setStatusBar({
+                    message: `information: ${m}`
+                  });
+                }}
+                onDoubleClick={e => void handleOpenRecord(e, item)}
                 title={
                   `${aOrAn} ${type}.
 tags: ${tagsToShow}
@@ -114,14 +155,18 @@ name: ${name}` || "null"
                 }
                 className="record-viewer-record record-box"
               >
-                {type}
+                {nameToShow}
               </div>
             );
           })}
         </section>
       </section>
       <footer className="record-viewer-footer">
-        <div>record count: {recordData.length}</div>
+        {statusBar ? (
+          <div>{statusBar.message}</div>
+        ) : (
+          <div>record count: {recordData.length}</div>
+        )}
       </footer>
     </div>
   );
