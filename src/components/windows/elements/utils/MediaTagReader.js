f79edbdfba12f92e7a5ad70542c327dee2ea2b8a
jpegzilla <eris@jpegzilla.com>
2020-03-17 19:21:52 -0400

    added media tag handlers

---
+++ src/components/windows/elements/utils/MediaTagReader.js
@@ -0,0 +1,159 @@
+import jDataView from "jdataview";
+import jsmediatags from "jsmediatags";
+import * as musicMetadata from "music-metadata-browser";
+import { audioTagSchema, imageTagSchema } from "./mediaTagSchema";
+import EXIF from "exif-js";
+
+export default class MediaTagReader {
+  constructor(mediaString) {
+    this.mediaString = mediaString;
+  }
+
+  async dataURItoBlob(dataURI) {
+    // convert base64 to raw binary data held in a string
+    const byteString = atob(dataURI.split(",")[1]);
+
+    // separate out the mime component
+    const mimeString = dataURI
+      .split(",")[0]
+      .split(":")[1]
+      .split(";")[0];
+
+    // write the bytes of the string to an ArrayBuffer
+    const ab = new ArrayBuffer(byteString.length);
+
+    // create a view into the buffer
+    const ia = new Uint8Array(ab);
+
+    // set the bytes of the buffer to the correct values
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    // write the ArrayBuffer to a blob, and you're done
+    const blob = new Blob([ab], { type: mimeString });
+    const buf = await blob.arrayBuffer();
+
+    return { blob, buffer: buf };
+  }
+
+  getAudioInfo() {
+    const { mediaString } = this;
+
+    return new Promise((resolve, reject) => {
+      this.dataURItoBlob(mediaString).then(e => {
+        this.view = new jDataView(e.buffer);
+
+        const { view } = this;
+
+        if (view.getString(3, view.byteLength - 128) === "TAG") {
+          const title = view.getString(30, view.tell());
+          const artist = view.getString(30, view.tell());
+          const album = view.getString(30, view.tell());
+          const year = view.getString(4, view.tell());
+
+          const metadata = {
+            title: title.trim(),
+            artist: artist.trim(),
+            album: album.trim(),
+            year: year.trim()
+          };
+
+          ["title", "artist", "album", "year"].forEach(i => {
+            // for some reason, this data sometimes has control characters in it. remove them
+
+            metadata[i] = metadata[i]
+              .replace(/[^\x00-\x7F]/g, "")
+              .replace(/[\x00-\x1F\x7F-\x9F]/g, "");
+          });
+
+          resolve({ status: "success", message: metadata });
+        } else {
+          resolve({ status: "failure", message: "no id3v1 data found" });
+        }
+      });
+    });
+  }
+
+  getFullAudioInfo(mime) {
+    const { mediaString } = this;
+    // fix this so that whatever lib is used to pull tags, the resulting tags
+    // are formatted in identically structured objects
+
+    return new Promise((resolve, reject) => {
+      this.dataURItoBlob(mediaString)
+        .then(res => {
+          // console.log("reading a file with type", mime);
+          // console.log("buffer is:", res.blob);
+
+          const parseWithJSMedia = [/mp3/gi, /image/gi, /video/gi];
+
+          if (parseWithJSMedia.some(e => e.test(mime))) {
+            jsmediatags.read(res.blob, {
+              onSuccess: meta => {
+                audioTagSchema(meta).then(res => {
+                  resolve({ status: "success", metadata: res });
+                });
+              },
+              onError: err => {
+                console.log(err);
+              }
+            });
+          } else {
+            // use this mainly for ogg / flac support
+            musicMetadata.parseBlob(res.blob).then(meta => {
+              audioTagSchema(meta).then(res => {
+                resolve({ status: "success", metadata: res });
+              });
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          reject({ status: "failure", message: err });
+        });
+    });
+  }
+
+  getFullImageInfo(mime) {
+    const { mediaString } = this;
+    // fix this so that whatever lib is used to pull tags, the resulting tags
+    // are formatted in identically structured objects
+
+    return new Promise((resolve, reject) => {
+      this.dataURItoBlob(mediaString)
+        .then(res => {
+          // console.log("reading a file with type", mime);
+          // console.log("buffer is:", res.blob);
+
+          EXIF.getData(res.blob, function() {
+            const tags = {};
+
+            for (const [k, v] of Object.entries(this.exifdata)) {
+              tags[k.toString().toLowerCase()] = v.toString().toLowerCase();
+            }
+
+            imageTagSchema(tags).then(res => {
+              resolve(res);
+            });
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          reject({ status: "failure", message: err });
+        });
+    });
+  }
+
+  static arrayBufferToBase64(buffer) {
+    let binary = "";
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+
+    return window.btoa(binary);
+  }
+}
---
+++ src/components/windows/elements/utils/mediaTagSchema.js
@@ -0,0 +1,151 @@
+// maybe use a worker for this
+
+import MediaTagReader from "./MediaTagReader";
+import { bytesToSize, secondsToTime } from "./../../../../utils/misc";
+
+export const imageTagSchema = tags => {
+  const tagSchema = {};
+  const msg = {};
+
+  const neededTags = [
+    "software",
+    "datetime",
+    "pixelxdimension",
+    "pixelydimension",
+    "xresolution",
+    "yresolution"
+  ];
+
+  try {
+    neededTags.forEach(item => {
+      if (item in tags) {
+        tagSchema[item] = tags[item];
+      }
+    });
+  } catch (err) {
+    msg.status = "failure";
+    msg.message = err;
+  }
+
+  if (msg.status === "failure") {
+  } else {
+    msg.status = "success";
+    msg.message = tagSchema;
+  }
+
+  return new Promise((resolve, reject) => {
+    if (msg.status === "failure") reject(msg.message);
+    else resolve(msg.message);
+  });
+};
+
+export const videoTagSchema = tags => {
+  const tagSchema = {};
+  const msg = {};
+
+  const neededTags = ["videoWidth", "videoHeight", "duration"];
+
+  try {
+    neededTags.forEach(item => {
+      if (item in tags) {
+        tagSchema[item] = tags[item];
+
+        if (item === "videoWidth") tagSchema["width"] = `${tags[item]}px`;
+
+        if (item === "videoHeight") tagSchema["height"] = `${tags[item]}px`;
+      }
+    });
+  } catch (err) {
+    msg.status = "failure";
+    msg.message = err;
+  }
+
+  tagSchema.duration = secondsToTime(parseInt(tagSchema.duration));
+
+  delete tagSchema["videoWidth"];
+  delete tagSchema["videoHeight"];
+
+  if (msg.status === "failure") {
+  } else {
+    msg.status = "success";
+    msg.message = tagSchema;
+  }
+
+  return new Promise((resolve, reject) => {
+    if (msg.status === "failure") reject(msg.message);
+    else resolve(msg.message);
+  });
+};
+
+export const audioTagSchema = tags => {
+  const tagSchema = {};
+  const msg = {};
+
+  const neededTags = [
+    "track",
+    "title",
+    "artist",
+    "year",
+    "date",
+    "albumartist",
+    "artists",
+    "album",
+    "picture",
+    "genre"
+  ];
+
+  try {
+    neededTags.forEach(item => {
+      if ("common" in tags) {
+        if (item in tags.common) {
+          if (item === "track")
+            tagSchema[item] = tags.common[item].no.toString().padStart(2, "0");
+          else if (item === "picture") tagSchema[item] = tags.common[item][0];
+          else tagSchema[item] = tags.common[item];
+        }
+      } else if ("tags" in tags) {
+        if (item in tags.tags) {
+          tagSchema[item] = tags.tags[item];
+        }
+      }
+
+      if ("date" in tagSchema) {
+        delete tagSchema.year;
+      }
+
+      if ("year" in tagSchema) {
+        tagSchema["date"] = tagSchema["year"];
+        delete tagSchema.year;
+      }
+
+      if (tagSchema[item] instanceof Array) tagSchema[item].join(", ");
+    });
+  } catch (err) {
+    msg.status = "failure";
+    msg.message = err;
+  }
+
+  if (msg.status === "failure") {
+  } else {
+    msg.status = "success";
+    msg.message = tagSchema;
+  }
+
+  if (tagSchema.picture) {
+    if (!(tagSchema.picture.data instanceof Uint8Array))
+      tagSchema.picture.data = Uint8Array.from(tagSchema.picture.data);
+
+    // .buffer is a property on the TypedArray prototype
+    const pictureBase64 =
+      "data:image/png;base64," +
+      MediaTagReader.arrayBufferToBase64(tagSchema.picture.data.buffer);
+
+    tagSchema.pictureData = pictureBase64;
+    tagSchema.pictureSize = bytesToSize(tagSchema.picture.data.byteLength);
+  }
+
+  return new Promise((resolve, reject) => {
+    if (msg.status === "failure") reject(msg.message);
+    else resolve(msg.message);
+  });
+};
