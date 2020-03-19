5d8225caa7987e693f553b4097ace1f777457526
jpegzilla <eris@jpegzilla.com>
2020-03-19 01:38:17 -0400

    minor formatting change, removed console log

--- src/components/subcomponents/ErrorBoundary.js
+++ src/components/subcomponents/ErrorBoundary.js
@@ -8,18 +8,17 @@ export default class ErrorBoundary extends Component {
 
   componentDidCatch(err, info) {
     this.setState({ error: true, info, message: err });
-
-    console.log(err, info);
   }
 
   render() {
-    if (this.state.error)
+    if (this.state.error) {
       return (
         <h1>
           there's an error here! =>{" "}
           {JSON.stringify(this.state.info.componentStack, null, 5)}
         </h1>
       );
+    }
     return this.props.children;
   }
 }
