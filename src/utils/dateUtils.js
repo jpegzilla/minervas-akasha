5c4fd390321efa27f314fe9109a4c1ba6a8d68b2
jpegzilla <eris@jpegzilla.com>
2020-03-20 05:55:07 -0400

    added natural date utility

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -9,9 +9,46 @@ const timesToMs = {
   now: Date.now()
 };
 
-const stringToMs = string => {
+/**
+ * naturalDate - takes in a natural language date string, outputs time in ms
+ *
+ * @param {string} string string such as "in 1 week and 2 days from now"
+ *
+ * @returns {number} date (in milliseconds since 1 jan 1970)
+ */
+export const naturalDate = string => {
   // format:
   // ex: 1 year from now
+  // ex: 2 weeks and 1 month from now
+  const timeNames = Object.keys(timesToMs);
+
+  const datesWithUnits = string
+    .match(/\d+(?:\s[^\d\s]+)/gi)
+    .map(item => item.trim());
+
+  const from =
+    string.endsWith("from today") ||
+    string.endsWith("from now") ||
+    string.startsWith("in");
+
+  let totalTime = 0;
+
+  datesWithUnits.forEach(item => {
+    const [num, type] = item.match(/\d+|\w+/gi);
+
+    const adjustedType = timeNames.find(n => new RegExp(n, "gi").test(type));
+
+    const ms = num * timesToMs[adjustedType];
+
+    totalTime += ms;
+  });
+
+  if (!from)
+    throw new Error(
+      "invalid time. must specify 'from now', 'from today', or 'in'."
+    );
+
+  return timesToMs.now + totalTime;
 };
 
 /**
