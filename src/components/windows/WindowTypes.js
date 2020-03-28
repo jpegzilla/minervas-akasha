d11696c58543831c71977811d0bc020935d4d58b
jpegzilla <eris@jpegzilla.com>
2020-03-27 22:14:29 -0400

    added new window types

--- src/components/windows/WindowTypes.js
+++ src/components/windows/WindowTypes.js
@@ -2,10 +2,14 @@ import { Console } from "./Console";
 import { DataStructure } from "./DataStructure";
 import { FileDialog } from "./FileDialog";
 import { RecordViewer } from "./RecordViewer";
+import { ImageViewer } from "./ImageViewer";
+import { Window } from "./Window";
 
 export const WindowTypes = {
   Console: Console,
   DataStructure: DataStructure,
   FileDialog: FileDialog,
-  RecordViewer: RecordViewer
+  RecordViewer: RecordViewer,
+  ImageViewer: ImageViewer,
+  Window: Window
 };
