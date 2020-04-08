84afd6b93fd299b93447152d3e8f2f7d26f01ff7
jpegzilla <eris@jpegzilla.com>
2020-04-08 19:28:28 -0400

    added 'reset record' command

---
+++ src/utils/commands/resetrecords.js
@@ -0,0 +1,7 @@
+import { MinervaMethods } from "./methods";
+
+export default (command, confirm = true) => {
+  if (!confirm) return MinervaMethods.verifyUser(confirm, command);
+
+  return MinervaMethods.verifyUser(confirm, null);
+};
