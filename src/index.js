7ab7d730038b305213c70a8ef33ccb3a4c6d85a7
jpegzilla <eris@jpegzilla.com>
2020-04-10 18:22:04 -0400

    service worker temp stuff

--- src/index.js
+++ src/index.js
@@ -10,8 +10,16 @@ import "./components/tasks/checkSupports";
 import "./components/tasks/setupExperiments";
 import "./components/tasks/setupPerformanceTesting";
 
+// service worker
+// import * as serviceWorker from "./serviceWorker";
+
 // for now, rendering a component that tells the user they can't use a mobile device
 // if they try to load the site on a mobile device
 if (mobilecheck())
   ReactDOM.render(<NoMobile />, document.getElementById("root"));
 else ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.register();
