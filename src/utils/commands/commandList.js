f52b1a4dfef3082800ebf15ad520296d98caf161
jpegzilla <eris@jpegzilla.com>
2020-04-18 16:25:04 -0400

    updated help command with new functionality

--- src/utils/commands/commandList.js
+++ src/utils/commands/commandList.js
@@ -2,6 +2,6 @@ export const commandDirectory = {
   "> help": "show this list.",
   "> reset records":
     "permanently remove all data structures. requires confirmation.",
-  "> add [structure type]":
-    "add a new structure. acceptable structure types:\r\n\t- shard\r\n\t- node\r\n\t- grimoire\r\n\t- athenaeum\r\n\t- hypostasis"
+  "> add [structure type] [name]":
+    "add a new structure of a certain type, with an\n    optional name. acceptable structure types:\r\n\t- shard\r\n\t- node\r\n\t- grimoire\r\n\t- athenaeum\r\n\t- hypostasis"
 };
