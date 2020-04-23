5bd9471fca266d13da34d70f315a8a7a6128eb42
jpegzilla <eris@jpegzilla.com>
2020-04-23 17:51:22 -0400

    updated help command

--- src/utils/commands/commandList.js
+++ src/utils/commands/commandList.js
@@ -2,6 +2,8 @@ export const commandDirectory = {
   "> help": "show this list.",
   "> reset records":
     "permanently remove all data structures. requires password. there is\n    no way to recover from this operation unless you exported your data beforehand.",
-  "> add [type] [name]":
-    "add a new structure of a certain type, with an optional name.\n    acceptable structure types:\r\n\tshard\t\tnode\t\tgrimoire\r\n\tathenaeum\thypostasis"
+  "> add <type> [name]":
+    "add a new structure of a certain type, with an optional name.\n    acceptable structure types:\r\n\tshard\t\tnode\t\tgrimoire\r\n\tathenaeum\thypostasis",
+  "> find [--type <structure type>] [--mime <mime type>] [query]":
+    "find a structure\n    using a search string, optionally using a mime type and / or structure type.\n    example: find music --type shard --mime audio/wav"
 };
--- src/utils/commands/help.js
+++ src/utils/commands/help.js
@@ -1,11 +1,14 @@
 import { commandDirectory } from "./commandList";
 
 export default () => {
-  let helpMessage = "minerva's akasha command list:\r\n";
+  let helpMessage = `minerva's akasha - command list:
+
+
+\r\n`;
 
   Object.entries(commandDirectory).forEach(([k, v], i, a) => {
-    if (i !== a.length - 1) helpMessage += `  ${k}: ${v}\r\n`;
-    else helpMessage += `  ${k}: ${v}`;
+    if (i !== a.length - 1) helpMessage += `  \n${k}: ${v}\r\n`;
+    else helpMessage += `  \n${k}: ${v}`;
   });
 
   return helpMessage;
