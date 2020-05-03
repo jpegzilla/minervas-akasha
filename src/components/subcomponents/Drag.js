5305015b7307da1653f3e7d2941e6e6026ee45cb
jpegzilla <eris@jpegzilla.com>
2020-05-03 17:54:45 -0400

    draggable window component base

---
+++ src/components/subcomponents/Drag.js
@@ -0,0 +1,14 @@
+import React, {
+  useState,
+  useEffect,
+  createElement,
+  cloneElement,
+  Children
+} from "react";
+
+// turns a component into a draggable box
+const Drag = props => {
+  const { children } = props;
+
+  return;
+};
