7c1600c98e6599ccb6068dfa311d3096a40e1b71
jpegzilla <eris@jpegzilla.com>
2020-05-23 15:04:34 -0400

    color refactor

--- src/utils/structures/utils/colorcodes.js
+++ src/utils/structures/utils/colorcodes.js
@@ -1,13 +1,18 @@
 // these color codes are applied to data structures' html representations as css
 // classes, which are used to apply colors to the elements.
 // each value in this structure corresponds to a certain css class defined in
-// _colorcodes.sass.
+// styles/components/windows/subwindows/_colorbox.sass.
 export default {
   pink: 'colorcode-pink',
   white: 'colorcode-white',
+  acapulco: 'colorcode-acapulco',
   blue: 'colorcode-blue',
-  red: 'colorcode-red',
+  mocha: 'colorcode-mocha',
   green: 'colorcode-green',
   purple: 'colorcode-purple',
-  yellow: 'colorcode-yellow'
+  yellow: 'colorcode-yellow',
+  rosevale: 'colorcode-rosevale',
+  pewterblue: 'colorcode-pewterblue',
+  husk: 'colorcode-husk',
+  lavender: 'colorcode-lavender'
 }
