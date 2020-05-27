1c31dece5b26b92f3a2820353bd31126d1dc49e2
jpegzilla <eris@jpegzilla.com>
2020-05-27 18:44:27 -0400

    textviewer version 1.0 done

--- src/components/windows/TextViewer.js
+++ src/components/windows/TextViewer.js
@@ -1,5 +1,27 @@
 import React from 'react'
 
 export default props => {
-  return <section>text viewer here</section>
+  const { text, humanSize, mime, title, alt } = props
+
+  console.log(props)
+
+  const altToShow = alt
+    .split('\n')
+    .splice(0, alt.split('\n').length - 1)
+    .join('\n')
+
+  const titleToShow = title.substring(0, 30).padEnd('0', 35)
+
+  return (
+    <section className='text-viewer-container'>
+      <header className='text-viewer-header'>
+        <span>{`text viewer - ${titleToShow}, ${humanSize}, ${mime}`}</span>
+      </header>
+      <div>
+        <pre className='text-viewer-text' title={altToShow}>
+          {text}
+        </pre>
+      </div>
+    </section>
+  )
 }
