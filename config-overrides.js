38e09cffaac3d08c783a5fa54a7b4809d58297ce
jpegzilla <eris@jpegzilla.com>
2020-05-29 18:27:33 -0400

    formatting changes

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,8 @@
-module.exports = function override(config, env) {
+module.exports = function override(config, _env) {
   config.module.rules.push({
     test: /\.worker\.js$/,
-    use: { loader: "worker-loader" }
-  });
-  config.output["globalObject"] = "this";
-  return config;
-};
+    use: { loader: 'worker-loader' }
+  })
+  config.output['globalObject'] = 'this'
+  return config
+}
