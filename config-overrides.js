656f21063c08d51bb2b102020ebaabc28659d68f
e <eli@heysparkbox.com>
2021-02-23 13:20:50 -0500

    trying to update all packages: mission one

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,12 @@
 module.exports = function override(config, _env) {
   config.module.rules.push({
     test: /\.worker\.js$/,
-    use: { loader: 'worker-loader' }
+    use: { loader: 'worker-loader' },
   })
+  config.externals = {
+    ...config.externals,
+    'react-native-fs': 'reactNativeFs',
+  }
   config.output['globalObject'] = 'this'
   return config
 }
